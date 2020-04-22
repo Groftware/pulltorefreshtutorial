ed4abe5a63bb96eb04feca44aec35c4e72678968
Chan Jing Hong <jinghongchan@gmail.com>
2020-04-22 15:07:39 +0800

    Added proptypes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "react/prop-types": "off"
+        "react/prop-types": "off",
+        "react/forbid-prop-types": "off"
     }
 };
